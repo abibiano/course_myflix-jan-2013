a9f861f6ac6fc5277a0739496668b058d02b0ca0
Alex Bibiano <alex@bibiano.es>
2013-02-04 16:03:51 +0100

    added user spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   validates :full_name, presence: true
   validates :password, presence: true
+  validates :email, uniqueness: true
 
 end
