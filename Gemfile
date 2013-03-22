cdb8085b8ac12290f31dac35a42abb871f199241
Alex Bibiano <alex@bibiano.es>
2013-03-22 12:10:13 +0100

    added image upload and S3 integration

--- Gemfile
+++ Gemfile
@@ -10,6 +10,9 @@ gem 'sidekiq'
 gem 'unicorn'
 gem "fabrication"
 gem "faker"
+gem 'carrierwave'
+gem "mini_magick"
+gem "fog", "~> 1.3.1"
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,28 +1,33 @@
 # config/unicorn.rb
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 2)
 timeout 15
 preload_app true
 
 @resque_pid = nil
 
 before_fork do |server, worker|
-  @resque_pid ||= spawn("bundle exec sidekiq")
+  spawn("bundle exec sidekiq")
 
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
 end
 
 after_fork do |server, worker|
+  Sidekiq.configure_client do |config|
+    config.redis = { :size => 1 }
+  end
+
+  Sidekiq.configure_server do |config|
+    config.redis = { :size => 5 }
+  end
 
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
 end
