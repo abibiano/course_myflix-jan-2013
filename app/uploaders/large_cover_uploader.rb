d27fe9a325fb0af36df27ff7d8c21fbf05eeb29f
Alex Bibiano <alex@bibiano.es>
2013-03-22 12:27:15 +0100

    added image upload and S3 integration

--- app/uploaders/large_cover_uploader.rb
+++ app/uploaders/large_cover_uploader.rb
@@ -3,7 +3,12 @@
 class LargeCoverUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
